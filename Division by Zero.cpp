b8a2383e00c6c53216950cf79f976467e02bd6e8
XArman222 <xsoulop@gmail.com>
2025-09-24 01:38:13 -0400

    Division by Zero.cpp

---
+++ Division by Zero.cpp
@@ -0,0 +1,29 @@
+//Arman Majhi
+// 24070123022
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b,ans;
+
+    cout<<"Enter two numbers: "<<endl;
+    cin>>a>>b;
+
+    try{
+        if(b==0){
+            throw b;
+        }
+        ans = a/b;
+        cout<<"Division is: "<<ans;
+    }
+    catch(float){
+        cout<<"Denominator cannot be zero.";
+}
+}
+
+// Output
+// Enter two numbers:
+// 5
+// 0
+// Denominator cannot be zero.
